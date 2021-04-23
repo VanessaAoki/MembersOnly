93a8e13ecb923975dd1b001f9840c09c01a6c05e
Vanessa Aoki <vanessa.aoki@outlook.com>
2021-04-23 19:25:03 -0300

    Add some validations to user

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :set_post, only: %i[ show edit update destroy ]
+  before_action :authenticate_user!, expect: %i[index show]
 
   # GET /posts or /posts.json
   def index
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_23_221645) do
+ActiveRecord::Schema.define(version: 2021_04_23_222322) do
 
   create_table "posts", force: :cascade do |t|
     t.text "post"
@@ -26,8 +26,11 @@ ActiveRecord::Schema.define(version: 2021_04_23_221645) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
 end
