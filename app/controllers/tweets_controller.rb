bc7408aa6748eb4392fe8d66412b69f0f5e75249
emmy <emmy66418@gmail.com>
2021-04-24 01:50:12 +0200

    Final touch for first section

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -4,6 +4,7 @@ class TweetsController < ApplicationController
   # GET /tweets or /tweets.json
   def index
     @tweets = Tweet.all.order("created_at DESC")
+    @tweet = Tweet.new
   end
 
   # GET /tweets/1 or /tweets/1.json
