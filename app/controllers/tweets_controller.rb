be0bd169da5dc77f042c37d618c7f48882125eee
emmy <emmy66418@gmail.com>
2021-04-24 00:11:18 +0200

    create navbar and form

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -3,7 +3,7 @@ class TweetsController < ApplicationController
 
   # GET /tweets or /tweets.json
   def index
-    @tweets = Tweet.all
+    @tweets = Tweet.all.order("created_at DESC")
   end
 
   # GET /tweets/1 or /tweets/1.json
